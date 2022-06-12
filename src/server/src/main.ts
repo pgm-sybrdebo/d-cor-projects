643897e66be6d5ae8ae2040ed109b43aadb0d87f
pgm-sybrdebo <sybren.deboever@student.arteveldehs.be>
2022-06-12 20:31:01 +0200

    changed PORT dynamic

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
     allowedHeaders:
       'Content-Type, Accept, Authorization, X-Requested-With, Origin, X-Csrftoken, X-Xsrftoken',
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
